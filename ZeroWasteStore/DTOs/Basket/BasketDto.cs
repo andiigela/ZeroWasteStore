47de6ca5e2fa9e79aed11a0b077364f619f5173a
drenzz1 <you@example.com>
2023-05-29 11:47:57 +0200

    Dreni commit

--- ZeroWasteStore/DTOs/Basket/BasketDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ZeroWasteStore.DTOs.Basket
-{
-    public class BasketDto
-    {
-        public int Id { get; set; }
-        public string BuyerId { get; set; }
-        public List<BasketItemDto> Items { get; set; }
-    }
-}
